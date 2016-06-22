365c40c03448be1222e02164af512d3a465fca52
Jonathan Leung <me@jonl.org>
2016-06-21 21:32:45 -0400

    make dots move sinusoidally

--- js/sketch.js
+++ js/sketch.js
@@ -2,14 +2,23 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-var NUM_VERTICL = 20;
+var NUM_VERTICAL = 20;
 var ELLIPSE_WIDTH = 30;
 var ELLIPSE_MARGIN = 20;
+var TOTAL_MS = 3000;
 
 function draw() {
-  for(var i=0; i<NUM_VERTICL; i++) {
-    var x = width/2;
+  clear(); //
+
+  for(var i=0; i<NUM_VERTICAL; i++) {
+    var θ = map(
+                (millis()+ i * (TOTAL_MS / NUM_VERTICAL)) % TOTAL_MS,
+                 0, 3000,
+                 0, TWO_PI
+               );
+    var x = cos(θ) * width/2.2 + width/2;
     var y = i * (ELLIPSE_WIDTH + ELLIPSE_MARGIN) + ELLIPSE_WIDTH + ELLIPSE_MARGIN;
+
     ellipse(x, y, ELLIPSE_WIDTH, ELLIPSE_WIDTH);
   }
 }
