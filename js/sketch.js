3216bca8a7e74bfc3f754a789ceb0e6db46309f1
Jonathan Leung <me@jonl.org>
2016-06-21 21:33:31 -0400

    draw radially spaced ellipses

--- js/sketch.js
+++ js/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-var NUM_VERTICAL = 20;
+var NUM_VERTICAL = 5;
 var ELLIPSE_WIDTH = 30;
 var ELLIPSE_MARGIN = 20;
 var TOTAL_MS = 3000;
@@ -10,15 +10,11 @@ var TOTAL_MS = 3000;
 function draw() {
   clear(); //
 
-  for(var i=0; i<NUM_VERTICAL; i++) {
-    var θ = map(
-                (millis()+ i * (TOTAL_MS / NUM_VERTICAL)) % TOTAL_MS,
-                 0, 3000,
-                 0, TWO_PI
-               );
-    var x = cos(θ) * width/2.2 + width/2;
-    var y = i * (ELLIPSE_WIDTH + ELLIPSE_MARGIN) + ELLIPSE_WIDTH + ELLIPSE_MARGIN;
+  var RADIUS = 100;
 
+  for(var θ = 0; θ <TWO_PI; θ += PI/6) {
+    var x = RADIUS * cos(θ) + width/2;
+    var y = RADIUS * sin(θ) + width/2;
     ellipse(x, y, ELLIPSE_WIDTH, ELLIPSE_WIDTH);
   }
 }
